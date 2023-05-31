d1e4674f22bb687196d5aaa04074ec412a71a993
Alexis Juarez <ajuarezs3@miumg.edu.gt>
2023-05-31 14:38:42 -0600

    vista

--- index.js
+++ index.js
@@ -23,8 +23,15 @@ app.set('view engine', '.hbs')
 
 
 app.use("/api", loginRoute);
+app.get('/',(req,res)=>{
+    res.render('login')
+});
 app.get('/login',(req,res)=>{
     res.render('login')
+});
+
+app.get('/home',(req,res)=>{
+    res.render('home')
 });
 
 
